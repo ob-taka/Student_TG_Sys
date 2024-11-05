318cda6ce76ac52f0ca600ac2e46736b9869ce7a
arandompiplup <arandompiplup@gmail.com>
2024-11-05 13:28:54 +0800

    added GPA validation function

--- main.py
+++ main.py
@@ -26,7 +26,36 @@ def group_students(stud_records):
         t_groups[stud.Tutorial_group].append(stud)
     return t_groups
 
-
+def validateStudents(array, TG):
+    group = 1
+    #print(TG)
+    TGmin = 10
+    TGmax = 0
+    classAve = 0
+    classTotal = 0
+    for i in array:
+       classTotal += float(i.CGPA)
+    classAve = classTotal / 50
+    while group < 11:
+        testArray = []
+        for i in array:
+            if i.group == group:
+                testArray.append(i)
+        minGPA = float(10)
+        maxGPA = float(0)
+        totalGPA = 0
+        for i in testArray:
+            floatCGPA = float(i.CGPA)
+            totalGPA += floatCGPA
+        gpaAve = totalGPA / 5
+        if gpaAve > TGmax:
+            TGmax = gpaAve
+        elif gpaAve < TGmin:
+            TGmin = gpaAve
+        #print(f"group {group}, gpaAve {gpaAve}")
+        group += 1
+    print(f"{TG} range is {round((TGmax - classAve), 2)}, or {round((classAve - TGmin), 2)}")
+        
 
 stud_records = read_records() # returns a list of Student objects
 stud_TG = group_students(stud_records) # returns a dictionary where keys are tut groups and values are Student objs
@@ -37,7 +66,7 @@ for key, value in stud_TG.items(): #sorts students by GPA
     sortThis = value
     sortedList = sorted(sortThis, key=lambda student: student.CGPA)
     value = sortedList
-
+finalList = []
 for key, value in stud_TG.items():
     listBoys = []
     listGirls = []
@@ -64,7 +93,9 @@ for key, value in stud_TG.items():
             index = 0
             groupNo += 1
 
-    print_records(interleaved, 50)
+    #print_records(interleaved, 50)
+    finalList += interleaved
+    validateStudents(interleaved, key)
+#print_records(finalList, 6000)
 
-   # print(key, len(listBoys), len(listGirls))
                 

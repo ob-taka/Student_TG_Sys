d31b80c242c147a4ad9f7a4caf70a0735c95337c
arandompiplup <arandompiplup@gmail.com>
2024-11-12 23:11:56 +0800

    added documentation comments and cleaned up the code

--- main copy 2.py
+++ main copy 2.py
@@ -27,20 +27,19 @@ def group_students(stud_records):
     return t_groups
 
 def validateStudents(array, TG):
-    #input("press something")
     group = 1
     TGmin = 10
     TGmax = 0
     classAve = 0
     classTotal = 0
-    for i in array:
+    for i in array:                  #this bit finds the class GPA average for comparison
        classTotal += float(i.CGPA)
     classAve = classTotal / 50
     while group < 11:
         testArray = []
         boyCount = 0
         girlCount = 0
-        for i in array:
+        for i in array:                 #gender balance within the group
             if i.group == group:
                 testArray.append(i)
                 if i.Gender == "Male":
@@ -50,21 +49,18 @@ def validateStudents(array, TG):
         minGPA = float(10)
         maxGPA = float(0)
         totalGPA = 0
-        for i in testArray:
+        for i in testArray:               #finds average GPA of the group
             floatCGPA = float(i.CGPA)
             totalGPA += floatCGPA
         gpaAve = totalGPA / 5
-        if gpaAve > TGmax:
+        if gpaAve > TGmax:  #compares the average GPA of the group to the class average
             TGmax = gpaAve
         elif gpaAve < TGmin:
             TGmin = gpaAve
-        print(f"{TG}, Group {group} Male:Female = {boyCount}:{girlCount}")
+        print(f"{TG}, Group {group} Male:Female = {boyCount}:{girlCount}") #validation print output for group
         group += 1
     
-    #
-    # input("press")
-    #print(f"Group {TG} Male:Female = {boyCount}:{girlCount}")
-    print(f"{TG} range is {round((TGmax - classAve), 2)}, or {round((classAve - TGmin), 2)}. \n")
+    print(f"{TG} range is {round((TGmax - classAve), 2)}, or {round((classAve - TGmin), 2)}. \n") #validation print output for the whole group
 
 def print_attr(list, attr):
     for i in list:
@@ -80,7 +76,6 @@ def findAverage(list, attr):
 
 
 def sortingFunc(list, attr):
-    #print("running")
     lengthList = len(list)
     if lengthList <= 5: #if length of list is below a certain value, use bubble sort to sort it
         for again in range(0, lengthList*2):
@@ -103,8 +98,8 @@ def sortingFunc(list, attr):
                 halfList1.append(i)
             elif float(getattr(i, attr)) == valence: #if equal to average, goes to center list (this was implemented because the prev version had recursion depth issues where all values in the list were the same and kept getting kicked to the left list infinitely)
                 meanList.append(i)
-        halfList1 = sortingFunc(halfList1, attr)
-        halfList2 = sortingFunc(halfList2, attr)
+        halfList1 = sortingFunc(halfList1, attr) #recursively sorting left list
+        halfList2 = sortingFunc(halfList2, attr) #recursively sorting right list
         finalList = halfList1 + meanList + halfList2 #recombines the lists
     return finalList
                    
@@ -117,11 +112,9 @@ for key, value in stud_TG.items(): #sorts students by GPA
     sortThis = value
     sortedList = sortingFunc(value, "CGPA")
     value = sortedList
-    #print_records(value, 50)
-    #input("enter")
     
 finalList = []
-for key, value in stud_TG.items():
+for key, value in stud_TG.items(): #this part takes the males and females and interleaves them with one in ascending GPA order and the other in decending GPA order
     listBoys = []
     listGirls = []
     for i in value:
@@ -130,24 +123,27 @@ for key, value in stud_TG.items():
         else:
             listGirls.append(i)
     interleaved = []
-    minLen = min(len(listGirls), len(listBoys))
+    minLen = min(len(listGirls), len(listBoys)) #this is here so the program doesnt try to pop from an empty list and run into an out of bounds error
     for i in range(0,minLen*2):
         if i % 2 == 0:
             interleaved.append(listBoys.pop())
         else:
             interleaved.append(listGirls.pop(0))
-    interleaved = interleaved + listBoys + listGirls
+    interleaved = interleaved + listBoys + listGirls #combines the remaining ppl in the lists to the main list
     index = 0
     groupNo = 1
-    for i in interleaved:
+    for i in interleaved: #making the small groups
         i.group = groupNo
         index += 1
         if index == 5:
             index = 0
             groupNo += 1
 
-    finalList += interleaved
+    finalList += interleaved #appends the sorted list to the final big list of students. can be changed to edit a dictionary
     validateStudents(interleaved, key)
+
+input("print final list") #this is so you can actually read the validation before getting 6000 lines in your terminal
+print_records(finalList, 6000) 
 
 
                 

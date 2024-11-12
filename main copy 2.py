09bcb678ea1758843ecbf0307e9df8ca54d4780a
arandompiplup <arandompiplup@gmail.com>
2024-11-12 20:44:06 +0800

    main copy 2 is now the latest file. removed the recursion number thing (and hopefully fixed the infinite recursion issue - it works on my machine)

--- main copy 2.py
+++ main copy 2.py
@@ -43,10 +43,10 @@ def validateStudents(array, TG):
         for i in array:
             if i.group == group:
                 testArray.append(i)
-            if i.Gender == "Male":
-                boyCount += 1
-            if i.Gender == "Female":
-                girlCount += 1
+                if i.Gender == "Male":
+                    boyCount += 1
+                if i.Gender == "Female":
+                    girlCount += 1
         minGPA = float(10)
         maxGPA = float(0)
         totalGPA = 0
@@ -58,12 +58,13 @@ def validateStudents(array, TG):
             TGmax = gpaAve
         elif gpaAve < TGmin:
             TGmin = gpaAve
+        print(f"{TG}, Group {group} Male:Female = {boyCount}:{girlCount}")
         group += 1
     
     #
     # input("press")
-    print(f"Group {TG} Male:Female = {boyCount}:{girlCount}")
-    #print(f"{TG} range is {round((TGmax - classAve), 2)}, or {round((classAve - TGmin), 2)}.")
+    #print(f"Group {TG} Male:Female = {boyCount}:{girlCount}")
+    print(f"{TG} range is {round((TGmax - classAve), 2)}, or {round((classAve - TGmin), 2)}. \n")
 
 def print_attr(list, attr):
     for i in list:
@@ -78,63 +79,46 @@ def findAverage(list, attr):
     return ave
 
 
-def sortingFunc(list, attr, recursionNo):
+def sortingFunc(list, attr):
+    #print("running")
     lengthList = len(list)
-    #print_records(list, lengthList)
-    if lengthList <= 5:
+    if lengthList <= 5: #if length of list is below a certain value, use bubble sort to sort it
         for again in range(0, lengthList*2):
             for i in range(0, lengthList-1):
                 if float(getattr(list[i], attr)) > float(getattr(list[i+1], attr)):
                     list[i], list[i+1] = list [i+1], list[i]
             again +=1
         finalList = list
-        #print_records(list, lengthList)
-    elif recursionNo > 50:
-        return list, recursionNo  
+    elif len(list) == 0: #if list is empty (most likely due to all values being the same for the prev recursion and thus being kicked to middle list), just return the empty list
+        return list
     else:
-        valence = findAverage(list, "CGPA")
-        halfLen = lengthList // 2
-        #print(halfLen, lengthList)
-        #print_records(list, lengthList)
+        valence = round(findAverage(list, "CGPA"), 2) #round was added because floating point math of 4.1499999999995 was resulting in an infinite recursion where everything in the list was 4.15 but the average was 4.14999999995 so yea
         halfList1 = []
         halfList2 = []
-        #print("recursion", recursionNo)
+        meanList = []
         for i in list:
-            if float(getattr(i, attr)) > valence:
-                #print(getattr(i, attr), valence, "more")
+            if float(getattr(i, attr)) > valence: #if more than average, goes to right list 
                 halfList2.append(i)
-            elif float(getattr(i, attr)) < valence:
-                #print(getattr(i, attr), valence, "less")
+            elif float(getattr(i, attr)) < valence: #if less than average, goes to left list
                 halfList1.append(i)
-            elif float(getattr(i, attr)) == valence:
-                #print(getattr(i, attr), valence, "same")
-                halfList1.append(i)
-        #print("half1")
-        #print_records(halfList1, len(halfList1))
-        #print("half2")
-        #print_records(halfList2, len(halfList2))
-        halfList1, recursion1 = sortingFunc(halfList1, attr, recursionNo+1)
-        #print("half1 done")
-        halfList2, recursion2 = sortingFunc(halfList2, attr, recursionNo+1)
-        #print("half2 done")
-        recursionNo = recursion1 + recursion2
-        #print(type(halfList1), type(halfList2))
-        finalList = halfList1 + halfList2
-    return finalList, recursionNo
-            
-# currently very buggy, doesnt really sort properly. have not diagnosed the problem yet, if anyone can fix it please do shoot me a message on telegram        
+            elif float(getattr(i, attr)) == valence: #if equal to average, goes to center list (this was implemented because the prev version had recursion depth issues where all values in the list were the same and kept getting kicked to the left list infinitely)
+                meanList.append(i)
+        halfList1 = sortingFunc(halfList1, attr)
+        halfList2 = sortingFunc(halfList2, attr)
+        finalList = halfList1 + meanList + halfList2 #recombines the lists
+    return finalList
+                   
 
 stud_records = read_records() # returns a list of Student objects
 stud_TG = group_students(stud_records) # returns a dictionary where keys are tut groups and values are Student objs
 
-#print_records(stud_records, 100)
 
 for key, value in stud_TG.items(): #sorts students by GPA
     sortThis = value
-    sortedList, depth = sortingFunc(value, "CGPA", 0)
-    print("\n \n", depth, "no. of recursions")
+    sortedList = sortingFunc(value, "CGPA")
     value = sortedList
     #print_records(value, 50)
+    #input("enter")
     
 finalList = []
 for key, value in stud_TG.items():
@@ -147,7 +131,6 @@ for key, value in stud_TG.items():
             listGirls.append(i)
     interleaved = []
     minLen = min(len(listGirls), len(listBoys))
-    #print(minLen)
     for i in range(0,minLen*2):
         if i % 2 == 0:
             interleaved.append(listBoys.pop())
@@ -163,10 +146,8 @@ for key, value in stud_TG.items():
             index = 0
             groupNo += 1
 
-    #print_records(interleaved, 50)
     finalList += interleaved
     validateStudents(interleaved, key)
-    #print_attr(interleaved, "CGPA")
-#print_records(finalList, 6000)
+
 
                 
